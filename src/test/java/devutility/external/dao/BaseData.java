a97d5e4350224abdf760f83d66ef28b6492f44e9
su liang <fangyansu@163.com>
2017-10-24 17:43:47 +0800

    Update

--- src/test/java/devutility/external/dao/BaseData.java
+++
@@ -1,9 +0,0 @@
-package devutility.external.dao;
-
-public abstract class BaseData {
-	protected String host = "";
-	protected int port = 0;
-	protected String loginName = "";
-	protected String password = "";
-	protected String database = "";
-}
