0d7b7efda5f5e5cbe3d8227141f09275597d4f1d
su liang <fangyansu@163.com>
2017-10-26 18:17:58 +0800

    Update

--- src/test/java/devutility/external/service/data/json/JacksonService.java
+++ src/test/java/devutility/external/service/data/json/JacksonService.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import devutility.internal.models.Person;
+import devutility.internal.test.models.Person;
 import devutility.internal.test.BaseService;
 
 public class JacksonService extends BaseService {
