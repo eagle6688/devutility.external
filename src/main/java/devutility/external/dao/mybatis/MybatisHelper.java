f89debf17f72186831b6937a07fd9ef987a909db
su liang <fangyansu@163.com>
2017-12-06 18:25:45 +0800

    Update

---
+++ src/main/java/devutility/external/dao/mybatis/MybatisHelper.java
@@ -0,0 +1,68 @@
+package devutility.external.dao.mybatis;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import devutility.internal.base.SingletonFactory;
+import devutility.internal.cache.MemoryCache;
+
+public class MybatisHelper {
+	// region variables
+
+	private String config;
+
+	// endregion
+
+	// region constructor
+
+	public MybatisHelper(String config) {
+		this.config = config;
+	}
+
+	// endregion
+
+	// region get SqlSessionFactory
+
+	public SqlSessionFactory getSqlSessionFactory() {
+		SqlSessionFactory sqlSessionFactory = MemoryCache.get(config, SqlSessionFactory.class);
+
+		if (sqlSessionFactory != null) {
+			return sqlSessionFactory;
+		}
+
+		Object locker = SingletonFactory.create(config, Object.class);
+
+		synchronized (locker) {
+			if (sqlSessionFactory == null) {
+				try (InputStream inputStream = Resources.getResourceAsStream(config)) {
+					SqlSessionFactory temp = new SqlSessionFactoryBuilder().build(inputStream);
+					sqlSessionFactory = temp;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return sqlSessionFactory;
+	}
+
+	// endregion
+
+	// region get SqlSession
+
+	public SqlSession getSqlSession() {
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+
+		if (sqlSessionFactory == null) {
+			return null;
+		}
+
+		return sqlSessionFactory.openSession();
+	}
+
+	// endregion
+}
